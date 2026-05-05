1ed42a0035e08d23afb390f8e3fd3e086260b450
aidegl <aidegl666@gmail.com>
2026-05-05 11:42:04 +0800

    111

--- team/xiaofeng/workspace/project/fsj/webview-spa/vite.config.js
+++ team/xiaofeng/workspace/project/fsj/webview-spa/vite.config.js
@@ -1,6 +1,11 @@
 export default {
   server: {
     port: 3002,
-    host: true
+    host: true,
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    }
   }
 }
